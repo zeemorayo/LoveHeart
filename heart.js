8b1f9373eec412640bebd9c1ea716cdd8217bc25
Nifesi Ajikiola <153874112+zeemorayo@users.noreply.github.com>
2024-09-02 01:23:14 +0100

    Add files via upload

---
+++ heart.js
@@ -0,0 +1,10 @@
+var canvas = document.getElementById("canva");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// GL Context
+var gl = canvas.getContext('webgl');
+if(!gl){
+  console.error("Unable to ini tialize WebGL")
+}
